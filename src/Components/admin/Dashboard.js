34eb831303667bb44992316944c7bd721ec30054
unknown <monmaramba@yahoo.com>
2019-08-17 23:48:40 -0700

    dashboard almost complete

---
+++ src/Components/admin/Dashboard.js
@@ -0,0 +1,11 @@
+import React from "react";
+import AdminLayout from "../../HOC/AdminLayout";
+
+const Dashboard = () => {
+  return (
+    <AdminLayout>
+      <div className="user_dashboard">Welcome to the dashboard</div>;
+    </AdminLayout>
+  );
+};
+export default Dashboard;
