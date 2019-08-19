25fd6916f2f61aa444e095274f3e50587cdbef79
unknown <monmaramba@yahoo.com>
2019-08-18 19:39:41 -0700

    private route authentication complete

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,19 @@ import { Switch, Route } from "react-router-dom";
 import SignIn from "./Components/signin";
 import Home from "./Components/home";
 import Dashboard from "./Components/admin/Dashboard";
+import PrivateRoute from "./Components/authRoutes/privateRoutes";
 
 function Routes(props) {
   return (
     <Layout>
       <Switch>
-        <Route exact component={Dashboard} path="/dashboard" />
+        <PrivateRoute
+          {...props}
+          path="/dashboard"
+          exact
+          component={Dashboard}
+        />
+
         <Route exact component={SignIn} path="/sign_in" />
         <Route exact component={Home} path="/" />
       </Switch>
