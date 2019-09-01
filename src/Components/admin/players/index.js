a74009fe736f1b4db3efd7d120932bea75be545f
unknown <monmaramba@yahoo.com>
2019-08-31 18:57:13 -0700

    player edit page complete with endpoint

--- src/Components/admin/players/index.js
+++ src/Components/admin/players/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-import { reverseArray } from "../../ui/misc";
-import { DataSnapshot } from "@firebase/database";
+
 import { Link } from "react-router-dom";
 import AdminLayout from "../../../HOC/AdminLayout";
 
@@ -15,9 +14,70 @@ import CircularProgress from "@material-ui/core/CircularProgress";
 
 import { firebasePlayers } from "../../../firebase";
 import { firebaseLooper } from "../../ui/misc";
+import { reverseArray } from "../../ui/misc";
 
 export default class AdminPlayers extends Component {
+  state = {
+    isLoading: true,
+    players: []
+  };
+
+  componentDidMount() {
+    firebasePlayers.once("value").then(snapShot => {
+      const players = firebaseLooper(snapShot);
+
+      this.setState({
+        isLoading: false,
+        players: reverseArray(players)
+      });
+    });
+  }
+
   render() {
-    return <div>Players</div>;
+    return (
+      <AdminLayout>
+        <div>
+          <Paper>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>First Name</TableCell>
+                  <TableCell>Last Name</TableCell>
+                  <TableCell>Number</TableCell>
+                  <TableCell>Position</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {this.state.players
+                  ? this.state.players.map((player, i) => (
+                      <TableRow key={i}>
+                        <TableCell>
+                          <Link to={`/admin_players/add_players/${player.id}`}>
+                            {player.name}
+                          </Link>
+                        </TableCell>
+                        <TableCell>
+                          <Link to={`/admin_players/add_players/${player.id}`}>
+                            {player.lastname}
+                          </Link>
+                        </TableCell>
+                        <TableCell>{player.number}</TableCell>
+                        <TableCell>{player.position}</TableCell>
+                      </TableRow>
+                    ))
+                  : null}
+              </TableBody>
+            </Table>
+          </Paper>
+          <div className="admin_progress">
+            {this.state.isLoading ? (
+              <CircularProgress thickness={7} style={{ color: "#98c5e9" }} />
+            ) : (
+              ""
+            )}
+          </div>
+        </div>
+      </AdminLayout>
+    );
   }
 }
