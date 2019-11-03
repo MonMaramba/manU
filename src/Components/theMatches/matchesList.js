6440dabbe610db6486a4a953485acb9af5fce344
unknown <monmaramba@yahoo.com>
2019-11-02 22:40:53 -0700

    animation on matches page

--- src/Components/theMatches/matchesList.js
+++ src/Components/theMatches/matchesList.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { easePolyOut } from "d3-ease";
+import NodeGroup from "react-move/NodeGroup";
 
 export default class MatchesList extends Component {
   state = {
@@ -11,7 +13,51 @@ export default class MatchesList extends Component {
     });
   }
 
+  showMatches = () =>
+    this.state.matcheslist ? (
+      <NodeGroup
+        data={this.state.matcheslist}
+        keyAccessor={d => d.id}
+        start={() => ({
+          opacity: 0,
+          x: -200
+        })}
+        enter={(d, i) => ({
+          opacity: [1],
+          x: [0],
+          timing: { duration: 500, delay: i * 50, ease: easePolyOut }
+        })}
+        update={(d, i) => ({
+          opacity: [1],
+          x: [0],
+          timing: { duration: 500, delay: i * 50, ease: easePolyOut }
+        })}
+        leave={(d, i) => ({
+          opacity: [0],
+          x: [-200],
+          timing: { duration: 500, delay: i * 50, ease: easePolyOut }
+        })}
+      >
+        {nodes => (
+          <div>
+            {nodes.map(({ key, data, state: { x, opacity } }) => (
+              <div
+                key={key}
+                className="match_box_big"
+                style={{
+                  opacity,
+                  transform: `translate(${x}px)`
+                }}
+              >
+                Blah!
+              </div>
+            ))}
+          </div>
+        )}
+      </NodeGroup>
+    ) : null;
+
   render() {
-    return <div></div>;
+    return <div>{this.showMatches()}</div>;
   }
 }
